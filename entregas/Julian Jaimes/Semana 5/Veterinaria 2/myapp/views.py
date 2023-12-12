eb8623bfda379907eede37c74649c9c362431abc
Jottajaimes <julianjaimesotero@gmail.com>
2023-12-11 21:32:35 -0500

    actualizacion semana 5

--- entregas/Julian Jaimes/Semana 5/Veterinaria 2/myapp/views.py
+++ entregas/Julian Jaimes/Semana 5/Veterinaria 2/myapp/views.py
@@ -8,11 +8,53 @@ from django.http import (
     HttpResponsePermanentRedirect,
     HttpResponseRedirect,
 )
-
+from django.contrib.auth.decorators import login_required
 from myapp.models import Usuario
 from myapp.models import Cancion
 from myapp.models import Receta
 from myapp.models import Regalo
+
+
+@login_required
+def index(request):
+    messsage_base = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1> Programa de veterinaria 0.1 </h1>
+            <h2> 
+                Propietarios
+            </h2>
+            <p>
+            {}
+            </p>
+            <h2> 
+                Mascotas
+            </h2>
+            <p>
+            {}
+            </p>
+            
+        </body>
+        </html>
+    """
+    usuarios = ["Yurley", "Juan"]
+    mascotas = ["Manchitas", "patitas", "micho"]
+    
+    messsage = messsage_base.format(
+        usuarios, mascotas
+    )
+    # Your code is here
+    '''
+    nombre de los propietarios en un texto
+    '''
+    return HttpResponse(messsage)
+
 
 def Regalos(request):
     usuarios = Usuario.objects.all()
